e56755fa14ad52de4a2ff5f693f316e01813cc8b
LeeKangJin <himnae5181@naver.com>
2019-12-05 09:42:24 +0900

    Commit
    Commit

---
+++ SQL File/SQL_TableInfomation.sql
@@ -0,0 +1,6 @@
+select * from INFORMATION_SCHEMA.VIEWS -- View 정보
+
+select * from INFORMATION_SCHEMA.TABLES -- Table 정보
+
+select * from INFORMATION_SCHEMA.COLUMNS WHERE COLUMN_NAME LIKE '%JobPositionCode%' 
+-- 특정 칼럼 조회 
