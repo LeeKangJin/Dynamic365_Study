fc282980fc6ffc152b73b873911c8b97d1509ae6
LeeKangJin <himnae5181@naver.com>
2019-12-13 13:52:41 +0900

    Dynamics Report SQL
    Update of SQL Training

---
+++ SQL File/DynamicsReport.sql
@@ -0,0 +1,6 @@
+
+SELECT new_l_requestName FROM Incident  
+
+SELECT new_name  FROM new_it_request  -- 6월 이후 부터의 서비스 케이스는 해당 Database에 있음. 
+
+SELECT Name FROM Entity JOIN StringMap ON (Entity.ObjectTypeCode = StringMap.ObjectTypeCode)
