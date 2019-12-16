99d50876fea797da37de772fd3df03f7aad638b6
LeeKangJin <himnae5181@naver.com>
2019-12-16 17:51:51 +0900

    Update SSRS sql
    the last modifing... (may be?)

--- SQL File/DynamicsReport.sql
+++ SQL File/DynamicsReport.sql
@@ -1,17 +1,41 @@
 
-SELECT         C.AccountIdName, I.StateCode , IR.TimeSpent, R.new_p_request_item, R.new_txt_request_type_detail1, I.CreatedByName, I.OwnerIdName, D.new_l_parent_departmentName
-FROM           Incident               AS I 
-LEFT JOIN       incidentResolution      AS IR   ON I.IncidentId = IR.IncidentId 
-LEFT JOIN       Contact                        AS C    ON I.ContactId = C.ContactId 
-LEFT JOIN       new_request_type AS R    ON I.new_l_request_type = R.new_request_typeId
-LEFT JOIN       StringMap                      AS S    ON R.new_p_request_item = S.AttributeValue
-LEFT JOIN       new_department          AS D    ON I.Id = D.OwnerId
+ 
 
+ 
+--From, To Default 값 설정
+DECLARE @STARTDATE DATE = '2019-11-01', @ENDDATE DATE = '2019-12-14'
+
+SET @STARTDATE = (SELECT  DATEADD(wk, DATEDIFF(d, 0, getdate()) / 7 -1 , -1)    AS '저번주 일요일(시작)')
+SET @ENDDATE =  (SELECT  DATEADD(wk, DATEDIFF(d, 0, getdate()) / 7 -1 , 5)     AS '저번주 토요일(끝)'   )
+
+SELECT @STARTDATE, @ENDDATE
+
+SELECT                     
+            A.AccountId,
+                 C.AccountIdName, 
+                 A.new_txt_gw_code,
+                 R.new_p_request_item,
+                 p_request_item.Value AS 'new_p_request_item_label',
+                              p_request_item.DisplayOrder AS 'new_p_request_item_order',
+                 R.new_txt_request_type_detail1,
+                 R.new_txt_request_type_detail2,
+                 I.OwnerId,
+                 I.OwnerIdName,
+                 B.Name,
+                 요청cnt = 1,
+                 CASE WHEN I.stateCode = 0 THEN 1 ELSE 0 END AS 진행중cnt,
+                 CASE WHEN I.stateCode = 2 THEN 1 ELSE 0 END AS 취소cnt,
+                 CASE WHEN I.stateCode = 1 THEN 1 ELSE 0 END AS 해결됨cnt,
+                              TimeSpent,
+                 ISNULL(CONVERT(float, CASE WHEN CONVERT(float, IR.TimeSpent) > 1440 THEN (CONVERT(float, IR.TimeSpent)/480)  ELSE (CONVERT(float, IR.TimeSpent)/60)   END),0) AS 지원시간,
+                 SU.IsDisabled
+FROM                         Incident                                               AS I 
+LEFT JOIN                    incidentResolution                            AS IR    ON I.IncidentId = IR.IncidentId 
+INNER JOIN                    Contact                                    AS C     ON I.CustomerId = C.ContactId 
+INNER JOIN                    new_request_type                              AS R     ON I.new_l_request_type = R.new_request_typeId
+LEFT JOIN                        (SELECT ObjectTypeCode,AttributeValue,AttributeName,[Value],DisplayOrder FROM StringMap WHERE AttributeName = 'new_p_request_item' AND ObjectTypeCode = 10051) p_request_item ON R.new_p_request_item = p_request_item.AttributeValue
+INNER JOIN                    SystemUser                      AS SU    ON I.OwnerId = SU.SystemUserId
+INNER JOIN                    Account                         AS A     ON C.AccountId = A.AccountId
+INNER JOIN                    BusinessUnit                                     AS B     ON B.BusinessUnitId = I.OwningBusinessUnit
 WHERE 1=1
-AND (S.ObjectTypeCode = '112' OR S.ObjectTypeCode = '10051')
-AND (S.AttributeName = 'statecode' OR S.AttributeName = 'new_p_request_item')
-AND I.CreatedOn BETWEEN '2019-12-08' AND '2019-12-14'
-
-SELECT * FROM Incident WHERE CreatedOn BETWEEN '2019-12-15' AND '2019-12-17'
-
-SELECT * FROM Contact
+AND DATEADD(Hour,9,I.CreatedOn) BETWEEN @STARTDATE AND @ENDDATE
