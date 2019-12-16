d783c5319ab1ae5f9f0225321927b42257925920
LeeKangJin <himnae5181@naver.com>
2019-12-16 11:43:37 +0900

    Query Update

--- SQL File/DynamicsReport.sql
+++ SQL File/DynamicsReport.sql
@@ -1,48 +1,17 @@
 
-SELECT new_l_requestName FROM Incident  
+SELECT         C.AccountIdName, I.StateCode , IR.TimeSpent, R.new_p_request_item, R.new_txt_request_type_detail1, I.CreatedByName, I.OwnerIdName, D.new_l_parent_departmentName
+FROM           Incident               AS I 
+LEFT JOIN       incidentResolution      AS IR   ON I.IncidentId = IR.IncidentId 
+LEFT JOIN       Contact                        AS C    ON I.ContactId = C.ContactId 
+LEFT JOIN       new_request_type AS R    ON I.new_l_request_type = R.new_request_typeId
+LEFT JOIN       StringMap                      AS S    ON R.new_p_request_item = S.AttributeValue
+LEFT JOIN       new_department          AS D    ON I.Id = D.OwnerId
 
-SELECT new_name  FROM new_it_request  -- 6월 이후 부터의 서비스 케이스는 해당 Database에 있음. 
-
-SELECT * FROM Entity JOIN StringMap ON (Entity.ObjectTypeCode = StringMap.ObjectTypeCode)
-
-SELECT CreatedOn,* FROM Incident
-ORDER BY Incident.CreatedOn DESC
-
-
-SELECT StateCode FROM Incident
-
-SELECT * FROM StringMap WHERE ObjectTypeCode = '112' AND AttributeName = 'statecode'
--- 활성 해결됨 취소됨
-
-
-
-SELECT * FROM Incident
-WHERE OwnerIdName ='이 강진'
-
-
-SELECT * FROM incidentresolution  -- 해결됨 시간 찾는 enetity
-
-SELECT ParentCustomerIdName,StringMap.Value,TimeSpent FROM Incident AS I LEFT JOIN incidentResolution AS IR
-ON(I.IncidentId = IR.IncidentId) LEFT JOIN Contact AS C 
-ON(I.OwnerId =C.OwnerId) LEFT JOIN StringMap ON(I.StateCode = StringMap.AttributeValue) 
 WHERE 1=1
-AND StringMap.ObjectTypeCode = '112'
-AND StringMap.AttributeName = 'statecode'
-AND I.CreatedOn between '2019-12-01' and '2019-12-07'
+AND (S.ObjectTypeCode = '112' OR S.ObjectTypeCode = '10051')
+AND (S.AttributeName = 'statecode' OR S.AttributeName = 'new_p_request_item')
+AND I.CreatedOn BETWEEN '2019-12-08' AND '2019-12-14'
 
-SELECT * FROM Incident
-SELECT * FROM contact
+SELECT * FROM Incident WHERE CreatedOn BETWEEN '2019-12-15' AND '2019-12-17'
 
-
---  해당 결과 매개 변수로 넘김
-SELECT * FROM StringMap WHERE ObjectTypeCode = '112' AND AttributeName = 'statecode'
--- 활성 해결됨 취소됨
-
-
-
--- 해당 SELECT 문 VIEW로 만든뒤 COUNT해서 활용하기..
-SELECT ParentCustomerIdName,TimeSpent,I.StateCode FROM Incident AS I LEFT JOIN IncidentResolution AS IR  -- 해결 안됬어도 조인 해야되서
-ON(I.IncidentId = IR.IncidentId) LEFT JOIN Contact AS C
-ON(I.ContactId = C.ContactId)
-WHERE I.CreatedOn between '2019-12-01' and '2019-12-07'
-
+SELECT * FROM Contact
